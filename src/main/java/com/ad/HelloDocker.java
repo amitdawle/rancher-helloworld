a078663f56e87ac7f802f40d003c9859e57941c1
Amit Dawle <amit.dawle@ihsmarkit.com>
2020-01-26 11:20:15 +0000

    Changed to run a simple http server

--- src/main/java/com/ad/HelloDocker.java
+++ src/main/java/com/ad/HelloDocker.java
@@ -1,9 +1,29 @@
 package com.ad;
 
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 class HelloDocker{
 
- public static void main(String[] args){  
-  System.out.println("Running java app in docker, built by travis-ci.");  
- }  
+
+  public static void main(String[] args) throws IOException {
+   HttpServer server = HttpServer.create(new InetSocketAddress(8008), 0);
+   HttpContext context = server.createContext("/");
+   context.setHandler(HelloDocker::handleRequest);
+   server.start();
+  }
+
+  private static void handleRequest(HttpExchange exchange) throws IOException {
+   String response = "Hi there!";
+   exchange.sendResponseHeaders(200, response.getBytes().length);//response code and length
+   OutputStream os = exchange.getResponseBody();
+   os.write(response.getBytes());
+   os.close();
+  }
 
 }  
